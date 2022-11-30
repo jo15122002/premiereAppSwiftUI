7653efbf431903b93c8bc8be1c6d4829abeaaba5
digital <digital@iMac24-5.local>
2022-11-30 13:25:30 +0100

    init

---
+++ MaPremiereAppSwiftUi/MaPremiereAppSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaPremiereAppSwiftUiApp.swift
+//  MaPremiereAppSwiftUi
+//
+//  Created by digital on 30/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MaPremiereAppSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
